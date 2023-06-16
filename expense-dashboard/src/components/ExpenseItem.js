4b76928c673244e7221679eebe0966df3a65a5cd
Souradeep <jeetsouradeep@gmail.com>
2023-06-16 12:55:55 -0400

    Inirial push

---
+++ expense-dashboard/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>294</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
