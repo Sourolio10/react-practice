396cb999c2252ef2b1bd7228842fdc94533c1ef5
Souradeep <jeetsouradeep@gmail.com>
2023-06-20 13:00:25 -0400

    Daily Practice

--- expense-dashboard/src/components/Expenses.js
+++ expense-dashboard/src/components/Expenses.js
@@ -1,11 +1,22 @@
-
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from './Card';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+  const [filteredYear, selfFilteredYear] = useState('2020');
+  const filterChangeHandler = selectedYear =>{
+    selfFilteredYear(selectedYear);
+};
+
   return (
+
+    <div>
+   
+
     <Card className="expenses">
+    <ExpensesFilter selected = {filteredYear} onChangefilter={filterChangeHandler}/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -27,6 +38,7 @@ function Expenses(props) {
         date={props.items[3].date}
       />
     </Card>
+    </div>
   );
 }
 
