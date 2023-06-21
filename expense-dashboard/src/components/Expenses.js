0edb40326eef9b4bb6d43b1fe55726798455d923
Souradeep <jeetsouradeep@gmail.com>
2023-06-21 13:10:37 -0400

    Daily Practice

--- expense-dashboard/src/components/Expenses.js
+++ expense-dashboard/src/components/Expenses.js
@@ -6,16 +6,6 @@ import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
   const [filteredYear, selfFilteredYear] = useState('2020');
-  let filterInfoText = '2019, 2021 & 2022';
-  if (filteredYear === '2019'){
-    filterInfoText ='2020, 2021 & 2022';
-  }
-  else if (filteredYear === '2021'){
-    filterInfoText ='2019, 2020 & 2022';
-  }
-  else{
-    filterInfoText ='2019, 2020 & 2021';
-  }
   
   const filterChangeHandler = selectedYear =>{
     selfFilteredYear(selectedYear);
@@ -28,7 +18,7 @@ function Expenses(props) {
 
     <Card className="expenses">
     <ExpensesFilter selected = {filteredYear} onChangeFilter={filterChangeHandler}/>
-      <p>Data for years {filterInfoText} is hidden.</p>
+
       {props.items.map(expense => 
       <ExpenseItem  
         title={expense.title}
