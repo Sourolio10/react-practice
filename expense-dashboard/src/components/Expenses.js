0894ff6f31914dedb27e993746e7e5b34b036d2a
Souradeep <jeetsouradeep@gmail.com>
2023-06-21 13:29:22 -0400

    Daily Practice

--- expense-dashboard/src/components/Expenses.js
+++ expense-dashboard/src/components/Expenses.js
@@ -19,11 +19,12 @@ function Expenses(props) {
     <Card className="expenses">
     <ExpensesFilter selected = {filteredYear} onChangeFilter={filterChangeHandler}/>
 
-      {props.items.map(expense => 
-      <ExpenseItem  
+      {props.items.map((expense) => (
+      <ExpenseItem
+        key = {expense.id}  
         title={expense.title}
         amount={expense.amount}
-        date={expense.date}/>)}
+        date={expense.date}/>))}
     </Card>
     </div>
   );
