e1007b361dd1e023cc3edec5df749f69cb774e40
Souradeep <jeetsouradeep@gmail.com>
2023-07-02 13:40:04 -0400

    Daily practice

--- user-profile-error-message/src/components/Users/UsersList.js
+++ user-profile-error-message/src/components/Users/UsersList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "../UI/Card";
 import classes from './UserList.module.css'
+
 const UsersList = (props) => {
     return(
         <Card className = {classes.users}>
         <ul>
             {props.users.map(user => (
-            <li>
+            <li key={user.id}>
                 {user.name} ({user.age} years old)
             </li>))}
         </ul>
